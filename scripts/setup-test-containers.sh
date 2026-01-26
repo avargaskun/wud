7fd835552d19e1dc5d43c76dce779e49dcbb4873
Antonio Vargas <antonio.vargas@outlook.com>
2026-01-25 20:19:55 -0800

    chore: add logging to setup-test-containers.sh

--- scripts/setup-test-containers.sh
+++ scripts/setup-test-containers.sh
@@ -70,28 +70,33 @@ else
     echo "🚀 Starting test containers..."
 
     # ECR
+    echo "Pulling ECR test image $ECR_TARGET ..."
     $DOCKER_CMD run -d --name ecr_sub_sub_test --label 'wud.watch=true' $ECR_TARGET
 
     # GHCR
+    echo "Pulling GHCR test images ..."
     $DOCKER_CMD run -d --name ghcr_radarr --label 'wud.watch=true' --label 'wud.tag.include=^\d+\.\d+\.\d+\.\d+-ls\d+$' ghcr.io/linuxserver/radarr:5.14.0.9383-ls245
-
-    # GITLAB
-    $DOCKER_CMD run -d --name gitlab_test --label 'wud.watch=true' --label 'wud.tag.include=^v16\.[01]\.0$' registry.gitlab.com/gitlab-org/gitlab-runner:v16.0.0
-
-    # HUB
-    $DOCKER_CMD run -d --name hub_homeassistant_202161 --label 'wud.watch=true' --label 'wud.tag.include=^\d+\.\d+.\d+$' --label 'wud.link.template=https://github.com/home-assistant/core/releases/tag/${major}.${minor}.${patch}' homeassistant/home-assistant:2021.6.1
-
-    # GHCR Podinfo
     $DOCKER_CMD run -d --name ghcr_podinfo_500 --label 'wud.watch=true' --label 'wud.tag.include=^6\.0\.0$' ghcr.io/stefanprodan/podinfo:5.0.0
     $DOCKER_CMD run -d --name ghcr_podinfo_latest --label 'wud.watch=true' --label 'wud.watch.digest=true' --label 'wud.tag.include=^latest$' ghcr.io/stefanprodan/podinfo:latest
 
+    # GITLAB
+    echo "Pulling Gitlab test images ..."
+    $DOCKER_CMD run -d --name gitlab_test --label 'wud.watch=true' --label 'wud.tag.include=^v16\.[01]\.0$' registry.gitlab.com/gitlab-org/gitlab-runner:v16.0.0
+
+    # HUB
+    echo "Pulling DockerHub test images ..."
+    $DOCKER_CMD run -d --name hub_homeassistant_202161 --label 'wud.watch=true' --label 'wud.tag.include=^\d+\.\d+.\d+$' --label 'wud.link.template=https://github.com/home-assistant/core/releases/tag/${major}.${minor}.${patch}' homeassistant/home-assistant:2021.6.1
+
     # LSCR
+    echo "Pulling LSCR test images ..."
     $DOCKER_CMD run -d --name lscr_radarr --label 'wud.watch=true' --label 'wud.tag.include=^\d+\.\d+\.\d+\.\d+-ls\d+$' lscr.io/linuxserver/radarr:5.14.0.9383-ls245
 
     # TrueForge
+    echo "Pulling TrueForge test images ..."
     $DOCKER_CMD run -d --name trueforge_radarr --label 'wud.watch=true' --label 'wud.tag.include=^v\d+\.\d+\.\d+$' --memory 512m --tmpfs /config oci.trueforge.org/containerforge/radarr:6.0.4
 
     # QUAY
+    echo "Pulling Quay test images ..."
     $DOCKER_CMD run -d --name quay_prometheus --label 'wud.watch=true' --label 'wud.tag.include=^v\d+\.\d+\.\d+$' --user root --tmpfs /prometheus:rw,mode=777 quay.io/prometheus/prometheus:v2.52.0
 
     echo "✅ Test containers started (9 containers)"
