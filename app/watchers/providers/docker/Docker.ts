d0838c4bf7e8b248f8163082f0102241cb30b52b
Antonio Vargas Garcia <avargaskun@gmail.com>
2026-02-15 09:05:15 -0800

    debug: add docker action name on failure

--- app/watchers/providers/docker/Docker.ts
+++ app/watchers/providers/docker/Docker.ts
@@ -355,7 +355,7 @@ class Docker extends Watcher {
                     }
                 } catch (e: any) {
                     this.log.debug(
-                        `Unable to get container details for container id=[${containerId}] (${e.message})`,
+                        `Unable to get container details for container action=[${action}] id=[${containerId}] (${e.message})`,
                     );
                 }
             }
